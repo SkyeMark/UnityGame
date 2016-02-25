fa64c9853c2074f432e3d95769d12a0ed0d5ce99
SkyeMark <skyebound404@gmail.com>
2016-02-25 16:46:12 -0600

    Checkpoint System
    The checkpoint system is a feature in the platform game I've been working on which saves the players progress at key locations, as well as providing a location for the player to respawn at. It uses ingame world coordinates which are usually tied to an ingame object that gives the player a visual for where the checkpoint exists.

---
+++ CheckPoint1.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Use PlayerPrefs.() to save all values
+// PlayerPrefs.SetInt("StringNameOfValue", IntegerValue);
+// PlayerPrefs.SetString("StringNameOfValue", "StringToSave");
+// PlayerPrefs.Save();  This saves all player pref values.
+public class CheckPoint1 : MonoBehaviour {
+	public bool Checkpoint1Activated = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnCheckpointActivate1(){ // Save the position values of Checkpoint1
+		PlayerPrefs.SetFloat ("PlayerProgressX", 284f); // X value for position
+		PlayerPrefs.SetFloat ("PlayerProgressY", 1.98f); // Y value for position
+		PlayerPrefs.SetInt("SceneToLoad", 1); // The scene to load 
+		// Found in: File > BuildSettings > IntegerValue Corresponding to scene
+		PlayerPrefs.Save(); // Save the values
+		Checkpoint1Activated = true; // This will stop the checkpoint from reactivating over and over
+
+	}
+
+}
