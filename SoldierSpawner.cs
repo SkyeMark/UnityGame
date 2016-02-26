a1f218a3ec4ed09759bdebf9e4acc42b16efa2ed
SkyeMark <skyebound404@gmail.com>
2016-02-25 23:21:25 -0600

    Soldier Spawner
    This script instantiates prefab enemies that then pursue the player. This is controlled by a timer, where totalSec is the variable that represents difference in time since the last spawn and the current time.

---
+++ SoldierSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoldierSpawner : MonoBehaviour {
+	public float totalSec;
+	public float timer;
+	public Rigidbody2D trooperToSpawn;
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		totalSec = Time.time - timer;
+		if (totalSec > 10) {
+			Rigidbody2D clone;
+			clone = Instantiate(trooperToSpawn, transform.position, transform.rotation) as Rigidbody2D;
+			clone.velocity = transform.TransformDirection (Vector2.right * 3);
+			timer = Time.time;
+
+		}
+
+
+		// Reset timer
+
+	}
+
+
+}
