c7a1ef713fbb045a0da710ac2ca9752f01a02581
SkyeMark <skyebound404@gmail.com>
2016-02-25 22:59:40 -0600

    Enemy Script
    - Holds the health stat for enemies 
    - Holds the ShowDamage() method for changing sprite color for visual cue
    - Calls the GameMaster to kill this object if health is zero

---
+++ Enemy.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public int Health = 100;
+	public GameObject thisEnemyObject;
+
+	[System.Serializable]
+	public class EnemyStats
+	{
+		public int Health = 100;
+	}
+	Animator anim;
+	public EnemyStats stats = new EnemyStats ();
+
+
+
+	public int fallBoundary = -40;
+
+	void Start () {
+		thisEnemyObject = gameObject;
+
+	}
+
+	public void DamageEnemy(int damage){
+		stats.Health -= damage;
+		if (stats.Health <= 0) {
+
+	 		Health = 0;
+			//GameMaster.KillEnemy (this);
+		}
+	}
+
+	// Called by Weapon.cs when damaging this enemy. It's set here so that we can 
+	// be sure we are calling the correct game object.
+	public void ShowDamage(){
+		thisEnemyObject.GetComponent<SpriteRenderer> ().color = new Color (1f, 0f, 0f);
+
+	}
+
+	void Update(){
+		thisEnemyObject.GetComponent<SpriteRenderer> ().color = new Color (255f, 255f, 255f);
+	}
+		
+}
